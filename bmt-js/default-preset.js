bd87645689ce4de48fe46e91a4b00a70ebda38f9
xiaoxuan010 <2592053474@qq.com>
2022-02-23 20:28:22 +0800

    更改默认设置文件格式

--- bmt-js/default-preset.js
+++ bmt-js/default-preset.js
@@ -5,4 +5,166 @@
  * 开源许可：GPL
  * 使用的开源代码：MDUI(https://github.com/zdhxiong/mdui)基于MIT协议
  */
-var default_preset = [{ "key_num": "0", "key_name": "KEY0", "status": "0%", "transition_time": "2", "content": [{ "num": "0", "person": "校管乐队", "name": "花车巡游" }, { "num": "1", "person": "校啦啦操队", "name": "青春•中国梦" }, { "num": "2", "person": "高一 8 班", "name": "守护者" }, { "num": "3", "person": "南海分校", "name": "星辰大海" }, { "num": "4", "person": "高二 5 班", "name": "曙光" }, { "num": "5", "person": "新世界分校", "name": "相信自己" }, { "num": "6", "person": "华海双语学校", "name": "Try everything" }, { "num": "7", "person": "番禺分校", "name": "芦花" }, { "num": "8", "person": "广州市杂技艺术剧院", "name": "肩上芭蕾：东方天鹅" }, { "num": "9", "person": "高二 6 班 黄巍仑", "name": "前奏曲" }, { "num": "10", "person": "高二 8 班 吕鹏宇、孔令 哲、徐煜翔、邢松涛 高二 10 班 王然铎", "name": "漫天繁星" }, { "num": "11", "person": "返校团队", "name": "待定" }, { "num": "12", "person": "校合唱团", "name": "Segalariak<br/>灯之河" }, { "num": "13", "person": "高一 6 班 陈志涛 高一 11 班 朱廷宇", "name": "学习趣谈" }, { "num": "14", "person": "In 乐社民乐团", "name": "步步高" }, { "num": "15", "person": "高一 9 班 廖若辰", "name": "良宵" }, { "num": "16", "person": "校工会", "name": "锦绣华“附”" }, { "num": "17", "person": "Heartflow 街舞社", "name": "以爱之名" }, { "num": "18", "person": "高二 10 班 胡宝淇 高一 10 班 谢伊玲 ", "name": "花之二重唱" }, { "num": "19", "person": "校舞蹈队 ", "name": "山河" }, { "num": "20", "person": "校武术队", "name": "武动乾坤" }, { "num": "21", "person": "高一 10 班 谢伊玲 高二 2 班鲍兴，李泽宽 高二 10 班胡宝淇 高三 1 班隋秉成 高三 3 班黄浩洋 高三 6 班石梓睿 高三 11 班邵伯阳", "name": "少年" }], "current_preset": "1" }, { "key_num": "1", "key_name": "KEY1", "status": "0%", "transition_time": "2", "content": [{ "num": "0", "person": "校管乐队", "name": "正线进行曲" }], "current_preset": "0" }, { "key_num": "2", "key_name": "KEY2", "status": "0%", "transition_time": "2", "content": [{ "num": "0", "person": "Key2", "name": "KEY2" }], "current_preset": "0" }, { "key_num": "3", "key_name": "KEY3", "status": "0%", "transition_time": "2", "content": [{ "num": "0", "person": "KEY3", "name": "key3" }], "current_preset": "0" }];
+var default_preset = [
+    {
+        "key_num":"0",
+        "key_name":"KEY0",
+        "status":"0%",
+        "transition_time":"2",
+        "content":[
+            {
+                "num":"0",
+                "person":"校管乐队",
+                "name":"花车巡游"
+            },
+            {
+                "num":"1",
+                "person":"校啦啦操队",
+                "name":"青春•中国梦"
+            },
+            {
+                "num":"2",
+                "person":"高一 8 班",
+                "name":"守护者"
+            },
+            {
+                "num":"3",
+                "person":"南海分校",
+                "name":"星辰大海"
+            },
+            {
+                "num":"4",
+                "person":"高二 5 班",
+                "name":"曙光"
+            },
+            {
+                "num":"5",
+                "person":"新世界分校",
+                "name":"相信自己"
+            },
+            {
+                "num":"6",
+                "person":"华海双语学校",
+                "name":"Try everything"
+            },
+            {
+                "num":"7",
+                "person":"番禺分校",
+                "name":"芦花"
+            },
+            {
+                "num":"8",
+                "person":"广州市杂技艺术剧院",
+                "name":"肩上芭蕾：东方天鹅"
+            },
+            {
+                "num":"9",
+                "person":"高二 6 班 黄巍仑",
+                "name":"前奏曲"
+            },
+            {
+                "num":"10",
+                "person":"高二 8 班 吕鹏宇、孔令 哲、徐煜翔、邢松涛 高二 10 班 王然铎",
+                "name":"漫天繁星"
+            },
+            {
+                "num":"11",
+                "person":"返校团队",
+                "name":"待定"
+            },
+            {
+                "num":"12",
+                "person":"校合唱团",
+                "name":"Segalariak<br/>灯之河"
+            },
+            {
+                "num":"13",
+                "person":"高一 6 班 陈志涛 高一 11 班 朱廷宇",
+                "name":"学习趣谈"
+            },
+            {
+                "num":"14",
+                "person":"In 乐社民乐团",
+                "name":"步步高"
+            },
+            {
+                "num":"15",
+                "person":"高一 9 班 廖若辰",
+                "name":"良宵"
+            },
+            {
+                "num":"16",
+                "person":"校工会",
+                "name":"锦绣华“附”"
+            },
+            {
+                "num":"17",
+                "person":"Heartflow 街舞社",
+                "name":"以爱之名"
+            },
+            {
+                "num":"18",
+                "person":"高二 10 班 胡宝淇 高一 10 班 谢伊玲 ",
+                "name":"花之二重唱"
+            },
+            {
+                "num":"19",
+                "person":"校舞蹈队 ",
+                "name":"山河"
+            },
+            {
+                "num":"20",
+                "person":"校武术队",
+                "name":"武动乾坤"
+            },
+            {
+                "num":"21",
+                "person":"高一 10 班 谢伊玲 高二 2 班鲍兴，李泽宽 高二 10 班胡宝淇 高三 1 班隋秉成 高三 3 班黄浩洋 高三 6 班石梓睿 高三 11 班邵伯阳",
+                "name":"少年"
+            }
+        ],
+        "current_preset":"1"
+    },
+    {
+        "key_num":"1",
+        "key_name":"KEY1",
+        "status":"0%",
+        "transition_time":"2",
+        "content":[
+            {
+                "num":"0",
+                "person":"校管乐队",
+                "name":"正线进行曲"
+            }
+        ],
+        "current_preset":"0"
+    },
+    {
+        "key_num":"2",
+        "key_name":"KEY2",
+        "status":"0%",
+        "transition_time":"2",
+        "content":[
+            {
+                "num":"0",
+                "person":"Key2",
+                "name":"KEY2"
+            }
+        ],
+        "current_preset":"0"
+    },
+    {
+        "key_num":"3",
+        "key_name":"KEY3",
+        "status":"0%",
+        "transition_time":"2",
+        "content":[
+            {
+                "num":"0",
+                "person":"KEY3",
+                "name":"key3"
+            }
+        ],
+        "current_preset":"0"
+    }
+];
