b6db7e7a7f2113cfb6ab185293a1219dfc025efa
xiaoxuan010 <2592053474@qq.com>
2022-02-25 17:24:26 +0800

    引入MDUI工具库

--- bmt-js/source-script.js
+++ bmt-js/source-script.js
@@ -9,6 +9,7 @@
 //全局变量
 var preset;
 const preset_id = "hflive-bmt-preset";
+var $ = mdui.$;
 
 //监听数据变动
 window.addEventListener('storage', RefreshContent);//有本地数据变动时调用RefreshContent函数
@@ -32,10 +33,11 @@ function RefreshContent() {
     for (var i = 0; i < 4; i++) {   //重复4次，每次设置一个key 
         current_preset = preset[i].current_preset;  //当前使用的预设
         current_content = preset[i].content[current_preset];
-        document.getElementById('key' + i + '-name').innerHTML = current_content.name;  //设置“节目名”或“歌词”字符串
+        // document.getElementById(`key${i}-name`).innerHTML = current_content.name;  //设置“节目名”或“歌词”字符串
+        $(`#key${i}-name`).html(current_content.name);
         if (i < 2)
-            document.getElementById('key' + i + '-person').innerHTML = current_content.person;  //设置“表演者”字符串
-
+            // document.getElementById('key' + i + '-person').innerHTML = current_content.person;  //设置“表演者”字符串
+            $(`#key${i}-person`).html(current_content.person);
         /* //以下设置转场时间（已弃用）
         var transision_time_style = document.createElement('style');  //创建一个style标签
         transision_time_style.id = 'bmt-key' + i + 'transision-time';   //设置标签的id
@@ -50,22 +52,26 @@ function RefreshContent() {
         } */
 
         //设置转场时间
-        var transition_time_style_html
-        if (preset[i].status == 'CLOSING' || preset[i].status == 'OPENING') 
-            transition_time_style_html = '.bmt-key' + i + '-transition {transition: ' + preset[i].transition_time + 's ease;}';
-        else
-        transition_time_style_html = '.bmt-key' + i + '-transition {transition: unset;}';
-        var transision_time_style_elem = document.getElementById('bmt-key' + i + 'transision-time');
-        if (transision_time_style_elem != undefined){
-            transision_time_style_elem.innerHTML= transition_time_style_html;
+        // var transition_time_style_html;
+        if (preset[i].status == 'CLOSING' || preset[i].status == 'OPENING') {
+            // transition_time_style_html = '.bmt-key' + i + '-transition {transition: ' + preset[i].transition_time + 's ease;}';
+            $(`#key${i}>*`).css('transition', `ease ${preset[i].transition_time}s`);
         }
         else{
+            // transition_time_style_html = '.bmt-key' + i + '-transition {transition: unset;}';
+            $(`#key${i}>*`).css('transision',`unset`);
+        }
+        /* var transision_time_style_elem = document.getElementById('bmt-key' + i + 'transision-time');
+        if (transision_time_style_elem != undefined) {
+            transision_time_style_elem.innerHTML = transition_time_style_html;
+        }
+        else {
             var transition_time_style = document.createElement('style');
             transition_time_style.id = 'bmt-key' + i + 'transision-time';
             transition_time_style.innerHTML = transition_time_style_html;
             document.getElementsByTagName('html')[0].appendChild(transition_time_style);
-        }
-        
+        } */
+
         // document.getElementsByTagName('html')[0].removeChild(document.getElementById('bmt-key' + i + 'transision-time'));
 
         //设置状态
